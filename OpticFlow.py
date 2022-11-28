eec5aaeac51e1bb703e3fcc46179cd68a9044bf9
powersleo <powersleo@gmail.com>
2022-11-27 18:08:39 -0800

    finished grid view

--- OpticFlow.py
+++ OpticFlow.py
@@ -22,7 +22,16 @@ def unitVector(vector1, vector2):
         return(x/magnitude, y/magnitude)
     else:
         return (0,0)
-    
+
+def gridView(image1, searchSize):
+    gridArray = []
+    imageShape = image1.shape
+    for i in range(0,imageShape[0],searchSize):
+        for j in range(0,imageShape[1],searchSize):
+            # grid = np.array([(i,j)], axis=2)
+            gridArray += [(i,j)]
+    return gridArray
+
 def OpticFlow(frame1, frame2, cornerList):
     
 
@@ -31,7 +40,7 @@ def OpticFlow(frame1, frame2, cornerList):
     # [-1,1]
     # [-1,1]
 
-    convolveFilterY = np.array([[1, 1], [-1, -1]]) 
+    convolveFilterY = np.array([[-1, -1], [1, 1]]) 
     # [-1,-1]
     # [1 , 1]
 
@@ -98,14 +107,14 @@ def OpticFlow(frame1, frame2, cornerList):
     # # [1,1,1]
 
 
-    convolveFilterX = convolveFilterX
-    convolveFilterY = convolveFilterY 
+    convolveFilterX = convolveFilterX * 2 
+    convolveFilterY = convolveFilterY * 2
     convolveFilterT = convolveFilterT 
     
     # #image convolve for gradient 
-    frameX = cv2.filter2D(frame2, -1, convolveFilterX) + cv2.filter2D(frame1, -1, convolveFilterX) 
-    frameY = cv2.filter2D(frame2, -1, convolveFilterY) + cv2.filter2D(frame1, -1, convolveFilterY)
-    frameT =  cv2.filter2D(frame1, -1, convolveFilterTi) - cv2.filter2D(frame2, -1, convolveFilterT)
+    frameX = cv2.filter2D(frame1, -1, convolveFilterX)
+    frameY = cv2.filter2D(frame1, -1, convolveFilterY)
+    frameT =  cv2.filter2D(frame1, -1, convolveFilterT) -  cv2.filter2D(frame2, -1, convolveFilterT)
     # frameT = frame1 - frame2
 
 
@@ -125,10 +134,10 @@ def OpticFlow(frame1, frame2, cornerList):
     
 
     for corners in cornerList: 
-        i, j = corners.ravel()
+        # print(corners)
+        i, j = corners
         i, j = int(i),int(j)
-        # print("this is i, j")
-        # print(i,j)
+        
         # print("\n")
          #determine size of search
         I_x = frameX[i-w:i+w + 1, j-w:j+w + 1].flatten()
@@ -140,20 +149,23 @@ def OpticFlow(frame1, frame2, cornerList):
         
         
         A = np.vstack((I_x, I_y)).T
-        
         AAT =np.dot(A, A.T)
         AATinv = np.linalg.pinv(AAT)
         ATB = np.dot(A.T, b)
         AATinvAt = np.dot(AATinv, A)
         U = np.dot(AATinvAt.T, b)
-        
         # print(b,"\n", A,"\n")
         # print("this is U",U)
         # print("end of U")
-        print(i,j)
-        if(i < frame1.shape[0] and j  < frame1.shape[1]):
-            u[i-1,j-1] = U[0][0]
-            v[i-1,j-1] = U[1][0]
+        # print(i,j)
+        if(i < frame1.shape[0] and j < frame1.shape[1] and i != 0 and j != 0):
+            u[i,j] = U[1][0]
+            v[i,j] = U[0][0]
+            if(U[0][0] != 0 and U[1][0] != 0):
+                print("Calculating at position: ")
+                print(i,j)
+                print("resultant vector = ",U[0][0],U[1][0] )
+            
     return (u,v)
        
 
@@ -168,17 +180,21 @@ frameWidthx = (int)(imagex/8)
 frameWidthy = (int)(imagey/12)
 frameNumberX = 1
 frameNumberY = 1
+gridOffset = 10
 frameInitial = frameInitial[(frameWidthx) * (frameNumberX - 1) + borderOffset:frameWidthx*frameNumberX, (frameWidthy) * (frameNumberY - 1) + borderOffset : frameWidthy*frameNumberY]
 frameInitialResize = cv2.resize(frameInitial,(frameInitial.shape[0] *3, frameInitial.shape[1]* 3))
 
 frameInitialGrey = cv2.cvtColor(frameInitialResize, cv2.COLOR_BGR2GRAY)
 frameInitialArray = np.array(frameInitialGrey)
 frameInitial_blur = cv2.GaussianBlur(frameInitialArray,(3,3), 0)
-cornerList = cv2.goodFeaturesToTrack(frameInitial_blur, 100, 0.01, 5)
+# cornerList = cv2.goodFeaturesToTrack(frameInitial_blur, 100, 0.01, 5)
+cornerList = gridView(frameInitial_blur, gridOffset)
+# print("corner list", cornerList)
 
 mask = np.zeros_like(frameInitialResize)
 writeFrame = frameInitialResize
 i = 0
+
 color = np.random.randint(0, 255, (100, 3))
 
 while(ret):
@@ -198,33 +214,43 @@ while(ret):
     
     #factor to scale vector by
     vectorScaleFactor = 7
+    colorIterator = 0
+    # print("this is corner ", cornerList)
     for corners in cornerList:
-        x = int(corners[0][0])
-        y = int(corners[0][1])
+        # x = int(corners[0][0])
+        # y = int(corners[0][1])
+        x,y = corners
+        x,y = int(x),int(y)
         # print(opticFlow[0][x,y])
         # print(opticFlow[1][x,y])
         # print((x,y))
         # print(opticFlow[0][x,y] ),y + int(opticFlow[1][x,y])
+        
         if(x < frameDeltaResize.shape[0] and y < frameDeltaResize.shape[1]):
-            print(unitVector((x-1,y-1), (x-1 + (opticFlow[0][x-1,y-1]  ),y-1 + (opticFlow[1][x-1,y-1]))))
-            normalizedVector = unitVector((x-1,y-1), (x-1 + int(opticFlow[0][x-1,y-1]  ),y-1 + int(opticFlow[1][x-1,y-1])))
-            
+            # print("writing arrow")
+            # print(unitVector((x-1,y-1), (x-1 + (opticFlow[0][x-1,y-1]  ),y-1 + (opticFlow[1][x-1,y-1]))))
+            normalizedVector = unitVector((x,y), (x + int(opticFlow[0][x,y]  ),y + int(opticFlow[1][x,y])))
             # print(type(normalizedVector))
-            if((x,y) != (x + normalizedVector[0],y + normalizedVector[1])):
-                print((x,y),(int(x + (normalizedVector[0]*vectorScaleFactor)),int(y + (normalizedVector[1]*vectorScaleFactor))))
-                print()
-                mask = cv2.arrowedLine(mask,(x,y), (int(x - (normalizedVector[0]*vectorScaleFactor)),int(y - (normalizedVector[1]*vectorScaleFactor))),color[i].tolist(),1, tipLength=.25)
-            if(i % 2 == 0 ):
-                mask = np.zeros_like(frameInitialResize)
-            print(i % 100)
-
-        img = cv2.add(frameDeltaResize, mask)    
+            if((x,y) != (x - normalizedVector[0],y + normalizedVector[1])):
+                # print((x,y),(int(x + (normalizedVector[0]*vectorScaleFactor)),int(y + (normalizedVector[1]*vectorScaleFactor))))
+                # print()
+                # print("writing arrow")
+                mask = cv2.arrowedLine(mask,(y,x), (int(y + (normalizedVector[1]*vectorScaleFactor)),int(x - (normalizedVector[0]*vectorScaleFactor))),color[colorIterator].tolist(),1, tipLength=.25)
+            # if(i % 2 == 0 ):
+                
+            # print(i % 100)
+        
+        img = cv2.add(frameDeltaResize, mask)
         img = cv2.resize(img, (1000, 1000))
     cv2.imshow('frame', img)
-    k = cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    fileName = "frames/frame" + str(i) + ".jpeg"
+    cv2.imwrite(fileName, img)
+    k = cv2.waitKey(1000)
+    mask = np.zeros_like(frameInitialResize)    
+    # cv2.destroyAllWindows()
     if k == 27:
         break
     i += 1
+    colorIterator += 1
     frameInitial_blur = frameDelta_blur
-    cornerList = cv2.goodFeaturesToTrack(frameDeltaGrey, 100, 0.01, 5)
+    cornerList = gridView(frameDelta_blur, gridOffset)
